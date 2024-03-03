314f0033196de63b23bb85f94943f866db625c9c
Seán O'Brien <sean95ob@gmail.com>
2024-03-03 15:46:13 +0000

    Complete second challeneg add css to sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,5 +1,7 @@
+import css from "./css/Sidebar.module.css"
+
 function Sidebar() {
-    return <div>
+    return <div className={css.sidebar}>
         <a>My Photos</a>
         <a>My Illustrations</a>
         <a>My Paintings</a>
